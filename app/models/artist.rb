e50eb2a11e165f88911a78d211bedf6b62129ac3
olinelson <olivernelson@icloud.com>
2019-04-21 20:38:00 -0400

    added test search class method for artist

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,11 @@
 class Artist < ApplicationRecord
     has_many :events, primary_key: "bands_in_town_id", foreign_key: "artist_id"
+
+
+    def test_search(name)
+        artist_name = URI.encode(name)
+        url = "https://rest.bandsintown.com/artists/#{artist_name}/?app_id=#{ENV['APP_ID']}"
+        response = HTTP.get(url).to_s
+        puts  response
+    end
 end
